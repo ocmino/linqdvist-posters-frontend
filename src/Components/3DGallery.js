2a419154926e2e9d7e7fcbe4a49974a1ffd79072
Jonathan <ocmino@gmail.com>
2023-03-19 16:55:28 +0100

    styling

--- src/Components/3DGallery.js
+++ src/Components/3DGallery.js
@@ -68,10 +68,25 @@ const pexel = (id) =>
   `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
 const images = [
   // Front
-  { position: [0, -0.5, 1.5], rotation: [0, 0, 0], url: pexel(1103970), url: "https://cdn.shopify.com/s/files/1/2469/4477/products/the-mountain-1_540x.jpg?v=1541006626"},
+  {
+    position: [0, -0.5, 1.5],
+    rotation: [0, 0, 0],
+    url: pexel(1103970),
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/the-mountain-1_540x.jpg?v=1541006626",
+  },
   // Back
-  { position: [-0.8, -0.5, -0.6], rotation: [0, 0, 0], url: pexel(416430), url: "https://cdn.shopify.com/s/files/1/2469/4477/products/vinterns-magi-1_540x.jpg?v=1568752238" },
-  { position: [0.8, -0.5, -0.6], rotation: [0, 0, 0], url: pexel(310452), url: "https://cdn.shopify.com/s/files/1/2469/4477/products/dalahast-if-1_540x.jpg?v=1564661546" },
+  {
+    position: [-0.8, -0.5, -0.6],
+    rotation: [0, 0, 0],
+    url: pexel(416430),
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/vinterns-magi-1_540x.jpg?v=1568752238",
+  },
+  {
+    position: [0.8, -0.5, -0.6],
+    rotation: [0, 0, 0],
+    url: pexel(310452),
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/dalahast-if-1_540x.jpg?v=1564661546",
+  },
   // Left
   {
     position: [-1.75, -0.5, 0.25],
@@ -81,28 +96,28 @@ const images = [
   {
     position: [-2.15, -0.5, 1.5],
     rotation: [0, Math.PI / 2.5, 0],
-    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/under-korsbarstradet-host-1_540x.jpg?v=1517500648"
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/under-korsbarstradet-host-1_540x.jpg?v=1517500648",
   },
   {
     position: [-2, -0.5, 2.75],
     rotation: [0, Math.PI / 2.5, 0],
-    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/styckningsschema-alg-1_540x.jpg?v=1517500102"
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/styckningsschema-alg-1_540x.jpg?v=1517500102",
   },
   // Right
   {
     position: [1.75, -0.5, 0.25],
     rotation: [0, -Math.PI / 2.5, 0],
-    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/stad-i-ljus-1_540x.jpg?v=1533906540"
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/stad-i-ljus-1_540x.jpg?v=1533906540",
   },
   {
     position: [2.15, -0.5, 1.5],
     rotation: [0, -Math.PI / 2.5, 0],
-    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/iceland-2-1_540x.jpg?v=1520528278"
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/iceland-2-1_540x.jpg?v=1520528278",
   },
   {
     position: [2, -0.5, 2.75],
     rotation: [0, -Math.PI / 2.5, 0],
-    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/skogsra-1_540x.jpg?v=1519238821"
+    url: "https://cdn.shopify.com/s/files/1/2469/4477/products/skogsra-1_540x.jpg?v=1519238821",
   },
 ];
 
--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,15 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { useMediaQuery } from "@mantine/hooks";
-import Cart from "./Cart/Cart";
-import QRCode from "qrcode.react";
-export default function Categories() {
-  const [cartItems, setCartItems] = useState([]);
-  const [modelUrl, setModelUrl] = useState("./Models/Poster.gltf");
-  const addToCart = (item) => {
-    setCartItems([...cartItems, item]);
-  };
 
+export default function Categories() {
   const styleContainer = {
     position: "absolute",
     top: "0",
@@ -17,7 +9,6 @@ export default function Categories() {
     width: "100vw",
     height: "100vh",
   };
-
   // Style for the title
   const styleAbout = {
     position: "absolute",
@@ -30,45 +21,26 @@ export default function Categories() {
     transition: "color 2s linear",
   };
 
-  const styleCategories = {
+  const stylePosters = {
     position: "absolute",
-    display: "flex",
-    justifyContent: "space-between",
-    top: "75%",
+    width: "75%",
+    top: "97.7%",
     left: "50%",
-    width: "80%",
     transform: "translate(-50%, -50%)",
-    color: "white",
-    fontSize: "1rem",
-    fontWeight: "bold",
-    transition: "color 2s linear",
-    padding: "20px",
-  };
-
-  const addToCartButton = {
-    padding: "10px",
-    borderRadius: "10px",
-    backgroundColor: "white",
-    color: "black",
-    border: "none",
-    cursor: "pointer",
-    fontSize: "1rem",
-    fontWeight: "bold",
-    transition: "all 0.3s ease",
-    outline: "none",
-    marginTop: "10px",
-    boxShadow: "0 0 10px 0 rgba(0, 0, 0, 0.2)",
+    display: "grid",
+    gridTemplateColumns: "repeat(3, 1fr)",
+    gridGap: "10px",
   };
 
   // Media query to change the font size
   const isMobile = useMediaQuery("(max-width: 600px)");
   if (isMobile) {
-    styleAbout.fontSize = "0.6rem";
-    styleAbout.top = "10%";
-    styleCategories.fontSize = "0.7rem";
-    styleCategories.flexDirection = "column";
-    styleCategories.top = "65%";
-    addToCartButton.fontSize = "0.5rem";
+    styleContainer.height = "100vh";
+    styleContainer.width = "100vw";
+    styleAbout.fontSize = "0.8rem";
+    stylePosters.fontSize = "0.8rem";
+    stylePosters.width = "85%";
+    stylePosters.top = "50%";
   }
 
   // Color state and interval timer to update the color
@@ -82,56 +54,42 @@ export default function Categories() {
 
   return (
     <div style={styleContainer}>
-      <Cart cartItems={cartItems} />
       <div style={{ ...styleAbout, color }}>
         <h1>Våra unika posters</h1>
       </div>
 
-      <div style={{ ...styleCategories, color }}>
-        <div>
-          <p1>Poster 1 - 299 SEK</p1>
-          <img
-            src="https://cdn.shopify.com/s/files/1/2469/4477/products/dalahast-if-1_360x.jpg?v=1564661546"
-            alt="Category 1"
-          />
-          <button
-            style={addToCartButton}
-            onClick={() => addToCart({ name: "Poster 1", price: 399 })}
-          >
-            Lägg till i kundvagnen
-          </button>
-        </div>
-
-        <div>
-          <p1>Poster 2 - 399 SEK</p1>
-
-          <img
-            src="https://cdn.shopify.com/s/files/1/2469/4477/products/imagination-loa-1_360x.jpg?v=1574715772"
-            alt="Category 1"
-          />
-          <button
-            style={addToCartButton}
-            onClick={() => addToCart({ name: "Poster 2", price: 399 })}
-          >
-            Lägg till i kundvagnen
-          </button>
-        </div>
-
-        <div>
-          <p1>Poster 3 - 249 SEK</p1>
-
-          <img
-            src="https://cdn.shopify.com/s/files/1/2469/4477/products/iceland-2-1_360x.jpg?v=1520528278"
-            alt="Category 1"
-          />
-          <button
-            style={addToCartButton}
-            onClick={() => addToCart({ name: "Poster 3", price: 249 })}
-          >
-            Lägg till i kundvagnen
-          </button>
-        </div>
-      </div>
+      <p style={{ ...stylePosters, color }}>
+        <img
+          src="https://cdn.shopify.com/s/files/1/2469/4477/products/the-mountain-1_540x.jpg?v=1541006626"
+          alt="poster1"
+          style={{ width: "95%" }}
+        />
+        <img
+          src="https://cdn.shopify.com/s/files/1/2469/4477/products/vinterns-magi-1_540x.jpg?v=1568752238"
+          alt="poster1"
+          style={{ width: "95%" }}
+        />
+        <img
+          src="https://cdn.shopify.com/s/files/1/2469/4477/products/imagination-loa-1_540x.jpg?v=1574715772"
+          alt="poster1"
+          style={{ width: "95%" }}
+        />
+        <img
+          src="https://cdn.shopify.com/s/files/1/2469/4477/products/the-mountain-1_540x.jpg?v=1541006626"
+          alt="poster1"
+          style={{ width: "95%" }}
+        />
+        <img
+          src="https://cdn.shopify.com/s/files/1/2469/4477/products/the-mountain-1_540x.jpg?v=1541006626"
+          alt="poster1"
+          style={{ width: "95%" }}
+        />
+        <img
+          src="https://cdn.shopify.com/s/files/1/2469/4477/products/the-mountain-1_540x.jpg?v=1541006626"
+          alt="poster1"
+          style={{ width: "95%" }}
+        />
+      </p>
     </div>
   );
 }
